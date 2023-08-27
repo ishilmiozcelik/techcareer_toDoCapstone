a830d3ba0e77b79d8700525e57816be5b39fc0c3
ismail h. ozcelik <iozcelyk@gmail.com>
2023-08-27 14:00:51 +0300

    todocapstone_functions

--- todolist-frontend/src/App.js
+++ todolist-frontend/src/App.js
@@ -2,68 +2,92 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import TaskList from './components/TaskList';
 import TaskInput from './components/TaskInput';
-import FilterButtons from './components/FilterButtons';
+import FooterButtons from './components/FilterButtons';
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const [filter, setFilter] = useState("all");
+    const [tasks, setTasks] = useState([]);
+    const [filter, setFilter] = useState("all");
 
-  useEffect(() => {
-    axios.get('http://localhost:8080/api/tasks')
-        .then(response => {
-          setTasks(response.data);
+    useEffect(() => {
+        axios.get('http://localhost:8080/api/tasks')
+            .then(response => {
+                setTasks(response.data);
+            });
+    }, []);
+
+    const handleAddTask = (name) => {
+        axios.post('http://localhost:8080/api/tasks', { name, completed: false })
+            .then(response => {
+                setTasks([...tasks, response.data]);
+            });
+    };
+
+    const handleToggleTask = (id) => {
+        const task = tasks.find(t => t.id === id);
+        const updatedTask = { ...task, completed: !task.completed };
+
+        axios.put(`http://localhost:8080/api/tasks/${id}`, updatedTask)
+            .then(response => {
+                const updatedTasks = tasks.map(t =>
+                    t.id === id ? response.data : t
+                );
+                setTasks(updatedTasks);
+            });
+    };
+
+    const handleDeleteTask = (id) => {
+        axios.delete(`http://localhost:8080/api/tasks/${id}`)
+            .then(() => {
+                const remainingTasks = tasks.filter(t => t.id !== id);
+                setTasks(remainingTasks);
+            });
+    };
+
+    const handleFilter = (type) => {
+        setFilter(type);
+    };
+
+    const handleDeleteDoneTasks = () => {
+        const doneTasks = tasks.filter(task => task.completed);
+        const deletePromises = doneTasks.map(task => axios.delete(`http://localhost:8080/api/tasks/${task.id}`));
+
+        Promise.all(deletePromises).then(() => {
+            const remainingTasks = tasks.filter(task => !task.completed);
+            setTasks(remainingTasks);
         });
-  }, []);
+    };
 
-  const handleAddTask = (name) => {
-    axios.post('http://localhost:8080/api/tasks', { name, completed: false })
-        .then(response => {
-          setTasks([...tasks, response.data]);
+
+    const handleDeleteAllTasks = () => {
+        const deletePromises = tasks.map(task => axios.delete(`http://localhost:8080/api/tasks/${task.id}`));
+
+        Promise.all(deletePromises).then(() => {
+            setTasks([]); // Clear all tasks from state after ensuring all deletions are complete
         });
-  };
+    };
 
-  const handleToggleTask = (id) => {
-    const task = tasks.find(t => t.id === id);
-    const updatedTask = { ...task, completed: !task.completed };
 
-    axios.put(`http://localhost:8080/api/tasks/${id}`, updatedTask)
-        .then(response => {
-          const updatedTasks = tasks.map(t =>
-              t.id === id ? response.data : t
-          );
-          setTasks(updatedTasks);
-        });
-  };
+    const filteredTasks = tasks.filter(task => {
+        if (filter === 'completed') return task.completed;
+        if (filter === 'todo') return !task.completed;
+        return true;
+    });
 
-  const handleDeleteTask = (id) => {
-    axios.delete(`http://localhost:8080/api/tasks/${id}`)
-        .then(() => {
-          const remainingTasks = tasks.filter(t => t.id !== id);
-          setTasks(remainingTasks);
-        });
-  };
-
-  const handleFilter = (type) => {
-    setFilter(type);
-  };
-
-  const filteredTasks = tasks.filter(task => {
-    if (filter === 'completed') return task.completed;
-    if (filter === 'todo') return !task.completed;
-    return true;
-  });
-
-  return (
-      <div className="App">
-        <TaskInput onAdd={handleAddTask} />
-        <TaskList
-            tasks={filteredTasks}
-            onToggle={handleToggleTask}
-            onDelete={handleDeleteTask}
-        />
-        <FilterButtons onFilter={handleFilter} />
-      </div>
-  );
+    return (
+        <div className="App">
+            <TaskInput onAdd={handleAddTask} />
+            <TaskList
+                tasks={filteredTasks}
+                onToggle={handleToggleTask}
+                onDelete={handleDeleteTask}
+            />
+            <FooterButtons
+                onFilter={handleFilter}
+                onDeleteDone={handleDeleteDoneTasks}
+                onDeleteAll={handleDeleteAllTasks}
+            />
+        </div>
+    );
 }
 
 export default App;
--- todolist-frontend/src/components/FilterButtons.js
+++ todolist-frontend/src/components/FilterButtons.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-function FilterButtons({ onFilter }) {
+function FilterButtons({ onFilter, onDeleteDone, onDeleteAll }) {
     return (
         <div>
             <button onClick={() => onFilter("all")}>All</button>
             <button onClick={() => onFilter("completed")}>Completed</button>
             <button onClick={() => onFilter("todo")}>Todo</button>
+            <button onClick={onDeleteDone}>Delete Done Tasks</button>
+            <button onClick={onDeleteAll}>Delete All Tasks</button>
         </div>
     );
 }
